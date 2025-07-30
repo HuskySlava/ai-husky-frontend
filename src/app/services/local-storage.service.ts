ea540691d961ab9ece9bc038b7bcaad41ccfdc4d
Slava Mihaelov <slava.m@pandats.com>
2025-07-30 21:16:49 +0300

    LocalStorage Service

---
+++ src/app/services/local-storage.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+const LS_PREFIX = 'ai-husky__';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class LocalStorageService {
+  private localeStorage: Storage;
+
+  constructor() {
+    this.localeStorage = window.localStorage;
+  }
+
+  public setKey(key: string, value: string): void {
+    this.localeStorage.setItem(LS_PREFIX + key, value);
+  }
+
+  public getKey(key: string) : string | null {
+    return this.localeStorage.getItem(LS_PREFIX + key);
+  }
+
+  public removeKey(key: string): void {
+    this.localeStorage.removeItem(LS_PREFIX + key);
+  }
+
+}
